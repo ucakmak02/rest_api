5e27312fd4dd896ff3a62dff715298a035886d84
ucakmak02 <u.s.cakmak02@gmail.com>
2019-08-20 16:37:05 +0300

    docker added

--- Api/api.py
+++ Api/api.py
@@ -98,4 +98,4 @@ api.add_resource(ForgotPassword,"/forgotPassword/<string:username>/<string:oldPa
 
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0",port=65432,debug=True)
+    app.run(host="0.0.0.0",debug=True)
